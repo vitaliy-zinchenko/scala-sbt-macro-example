6e3ffe75d3ee3f4e5913ab5bdc0ca98b816f0de3
vitaliy-zinchenko <zinjvi@gmail.com>
2017-01-09 14:46:27 +0200

    experimental

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,17 +1,18 @@
-object Test extends App {
+object Test {
 
   @getParamNames
   def ttt(v: String, v2: Int): Unit = {
     println("!!!" + v)
   }
 
-  println("The generated variable with list of parameter names: " + this.tttParams)
-
   @getParamNames
   def oneMoreMethod(v: String, v2: Int, oneMoreParam: String): Unit = {
     println("!!!" + v)
   }
 
-  println("The generated variable with list of parameter names: " + this.oneMoreMethodParams)
+  def main(args: Array[String]): Unit = {
+    println("The generated variable with list of parameter names: " + this.tttParams)
+    println("The generated variable with list of parameter names: " + this.oneMoreMethodParams)
+  }
 
 }
